45f32243a44b2dca00906afb19201bca6f4ca0b0
destiny <1848765519@qq.com>
2017-07-27 16:40:16 +0800

    dist

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        autoplayHoverPause: true
-    });
-});
